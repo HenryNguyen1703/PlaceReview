700b98f16764bca962ec3e8cf0aa8f01fd91bbb8
HenryNguyen1703 <nguyenngochieu170302004@gmail.com>
2025-08-22 01:36:27 +0700

    Config application.properties mysql

---
+++ src/main/java/vn/ngochieu/com/PlaceReviewApplication.java
@@ -0,0 +1,13 @@
+package vn.ngochieu.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceReviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaceReviewApplication.class, args);
+    }
+
+}
